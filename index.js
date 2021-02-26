214e545d79375a5ba5ff7ef234914fadb5b6ff90
jayvir1singh <68712699+jayvir1singh@users.noreply.github.com>
2021-02-25 23:59:26 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -422,13 +422,3 @@ window.addEventListener("beforeinstallprompt", (e) => {
     pwaInstallInput.style.display = "block";
     pwaInstallInput.addEventListener("click", pwaInstallClick);
 });
-
-
-const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test( userAgent );
-}
-
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-  
-if(isIos() && !isInStandaloneMode()) this.setState({ showInstallMessage: true });
